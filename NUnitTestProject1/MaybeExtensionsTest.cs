3003fc50f241231ddd1b545847c1569ddf9f2701
Alexandr Kulitsky <to.kulitsky@outlook.com>
2016-04-07 01:23:00 +0300

    Replace tests with NUnit

---
+++ NUnitTestProject1/MaybeExtensionsTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using iJoyMonads;
+using NUnit.Framework;
+
+namespace NUnitTestProject1
+{
+    [TestFixture]
+    public class MaybeExtensionsTest
+    {
+        [Test]
+        public void TestGet()
+        {
+            var match = new Match
+            {
+                Team1 = new Team { Title = "Juventus" },
+                Team2 = null
+            };
+
+            Assert.Throws<NullReferenceException>(() => { var a = match.Team2.Title; });
+            Assert.IsNotNull(match.Get(x => x.Team1).Get(x => x.Title));
+            Assert.IsNull(match.Get(x => x.Team2).Get(x => x.Title));
+        }
+    }
+
+    public class Match
+    {
+        public Team Team1 { get; set; }
+        public Team Team2 { get; set; }
+    }
+
+    public class Team
+    {
+        public string Title { get; set; }
+    }
+}
