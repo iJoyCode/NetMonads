814b0eea1a47cdb915ca5227d809ffa411cb5752
Alexandr Kulitsky <to.kulitsky@outlook.com>
2016-04-06 23:04:39 +0300

    Add project files.

---
+++ iJoyMonads/MaybeExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace iJoyMonads
+{
+    public static class MaybeExtensions
+    {
+        public static TResult Get<TInput, TResult>(this TInput obj, Func<TInput, TResult> checker) 
+            where TInput: class 
+            where TResult: class => obj == null ? null : checker(obj);
+    }
+}
---
+++ iJoyMonadsTests/MaybeExtensionsTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using iJoyMonads;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace iJoyMonadsTests
+{
+    [TestClass]
+    public class MaybeExtensionsTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void TestGet()
+        {
+            var match = new Match
+            {
+                Team1 = new Team { Title = "Juventus" },
+                Team2 = null
+            };
+
+            var title = match.Team2.Title;
+
+            Assert.IsNotNull(match.Get(x => x.Team1).Get(x => x.Title));
+            Assert.IsNull(match.Get(x => x.Team2).Get(x => x.Title));
+
+            
+        }
+    }
+
+    #region Fixtures
+    public class Match
+    {
+        public Team Team1 { get; set; }
+        public Team Team2 { get; set; }
+    }
+
+    public class Team
+    {
+        public string Title { get; set; }
+    }
+    #endregion
+}
